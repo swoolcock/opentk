f8ade1252640922e51c222b1d1cc4b66860dd1f3
Shane Woolcock <samahnub@gmail.com>
2020-05-13 17:22:52 +0930

    Fix some missing files in projects

--- src/osuTK/ContextExistsException.cs
+++ src/osuTK/ContextExistsException.cs
@@ -47,15 +47,4 @@ namespace osuTK
         /// </summary>
         public override string Message { get; }
     }
-
-    /// <summary>
-    /// This exception is thown when a binding method is called and the bindings have not been rewritten by Rewrite.exe.
-    /// </summary>
-    public class BindingsNotRewrittenException : Exception
-    {
-        /// <summary>
-        /// Constructs a new BindingsNotRewrittenException instance.
-        /// </summary>
-        public BindingsNotRewrittenException() : base("Rewrite.exe has not been run.") { }
-    }
 }
